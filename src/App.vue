16a02e3a879b8bcf1632814edddc374ccb9e4e39
Aziz Sobirov <azizsobirovdev@gmail.com>
2021-09-23 07:51:32 +0500

    vue58

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@
         <img :src="img" :alt="city">
         <h2>{{ temp }}°C</h2>
         <p>{{ description }}</p>
-        <h4><a href="https://sobirov.netlify.app">Created By Aziz Sobirov</a></h4>
+        <ul>
+          <li><h4>Sunrise Time - {{sunrise}}</h4></li>
+          <li><h4>Sunset Time - {{sunset}}</h4></li>
+          <li><a href="https://sobirov.netlify.app">Created By Aziz Sobirov</a></li>
+        </ul>
       </div>
     </div>
 </template>
@@ -28,9 +32,11 @@ export default {
             img: '',
             temp: '',
             description: '',
-            input: 'hazorasp',
+            input: '',
             app_search:'app_search',
-            error: ''
+            error: '',
+            sunrise:'',
+            sunset:''
         }
     },
     methods: {
@@ -48,8 +54,20 @@ export default {
               this.temp = Math.floor(data.main.temp - 273.15)
               this.img = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
               this.description = data.weather[0].description
+              // sunrise
+              let unix_time = data.sys.sunrise
+              var date = new Date(unix_time * 1000);
+              var hours = date.getHours();
+              var minutes = "0" + date.getMinutes();
+              this.sunrise = hours + ':' + minutes.substr(-2);
+              // sunset
+              let unix_time2 = data.sys.sunset
+              var date2 = new Date(unix_time2 * 1000);
+              var hours2 = date2.getHours();
+              var minutes2 = "0" + date2.getMinutes();
+              this.sunset = hours2 + ':' + minutes2.substr(-2);
             })
-        }
+        },
     }
 }
 </script>
